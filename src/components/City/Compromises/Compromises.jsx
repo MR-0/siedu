f2e23b14778f3b949d25f1aec2595fd33b7486f2
mr0-spike <mr0@spikelab.xyz>
2021-07-26 19:37:43 -0400

    Remove communes chunks

--- src/components/City/Compromises/Compromises.jsx
+++ src/components/City/Compromises/Compromises.jsx
@@ -1,26 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useConfigValue } from 'contexts/Config';
 import { useDataValue } from 'contexts/Data';
 import { Compromise } from './Compromise';
-
-const addToChunk = (setter, arr, chunk) => {
-  const i = chunk.length;
-  const commune = arr[i];
-
-  if (commune) {
-    chunk = [ ...chunk, <Commune key={i} commune={commune} /> ];
-    setter && setter(chunk);
-    window.requestAnimationFrame(() => {
-      addToChunk(setter, arr, chunk)
-    });
-  }
-
-  return () => {
-    arr = [];
-    chunk = [];
-    setter = null;
-  }
-};
 
 export const Compromises = () => {
   const { commune } = useConfigValue();
